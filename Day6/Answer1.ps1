c2cda917fe8a08c16fee6671a5bb86f5071cd4d5
Beardhammer <60657565+Beardhammer@users.noreply.github.com>
2020-12-06 09:07:14 -0600

    Add files via upload

---
+++ Day6/Answer1.ps1
@@ -0,0 +1,16 @@
+﻿$list = get-content "C:\Users\user\Documents\adventofcode\day6\input.txt"
+$list += ""
+$count =0
+$questions = @()
+foreach($line in $list){
+if ($line -eq ""){
+        $questions = $questions | Sort-Object -Unique
+        $count += $questions.count-1
+        $questions = @()
+
+        }
+    else{
+    $questions += $line -split '' | sort
+    }
+}
+$count
---
+++ Day6/Answer2.ps1
@@ -0,0 +1,30 @@
+﻿$list = get-content "C:\Users\user\Documents\adventofcode\day6\input.txt"
+$list += ""
+$everyone = @()
+$questions = @()
+foreach($line in $list){
+    #group of people
+    if ($line -eq ""){
+        #letter array that at least 1 person answered yes to
+        $allletters = $questions -split ''| sort -Unique
+            #testing all people
+            foreach($person in $questions){
+                $splitperson = $person -split ''
+                #testing all letters
+                foreach($letter in $allletters){
+                    if($letter -notin $splitperson){
+                       #remove the letter if its not in a persons anwer
+                       $allletters = $allletters.replace($letter,'') 
+                    }
+                }
+            }
+        $everyone += $allletters
+        $questions = @()
+        
+        }
+    else{
+    $questions += $line | sort
+    }
+}
+#removing extra lines
+($everyone | ?{$_ -ne ""}).count
