15832399ef4e360df3c0543fbe07cb47f73a4eaf
Beardhammer <60657565+Beardhammer@users.noreply.github.com>
2020-12-08 10:02:15 -0600

    Add files via upload

---
+++ Day8/Answer1.ps1
@@ -0,0 +1,14 @@
+﻿$list = get-content "C:\Users\user\Documents\adventofcode\day8\input.txt"
+
+$raninstructions = @()
+$acc = 0
+:forloop for($x=0;$x -lt $list.Length;$x++){
+    if($x -in $raninstructions){break forloop}
+    $raninstructions += $x
+    switch -wildcard ($list[$x][0..2] -join ''){
+        "acc" { $acc += $list[$x].Substring(4,$list[$x].Length -4)}
+        "jmp" {$x += $list[$x].Substring(4,$list[$x].Length -4)-1}
+        
+    }
+}
+$acc
---
+++ Day8/Answer2.ps1
@@ -0,0 +1,25 @@
+﻿$list = get-content "C:\Users\user\Documents\adventofcode\day8\input.txt"
+$listindex=0
+do{
+$raninstructions = @()
+$acc = 0
+$templist = $list.clone()
+switch -Wildcard ($templist[$listindex][0..2] -join ''){
+    "jmp" {$templist[$listindex]=$list[$listindex].Replace('jmp','nop')}
+    "nop" {$templist[$listindex]=$list[$listindex].Replace('nop','jmp')}
+    "acc" {continue}
+
+}
+$listindex++
+:forloop for($x=0;$x -lt $templist.Length;$x++){
+    if($x -in $raninstructions){break forloop}
+    $raninstructions += $x
+        switch -wildcard ($templist[$x][0..2] -join ''){
+            "acc" { $acc += $templist[$x].Substring(4,$templist[$x].Length -4)}
+            "jmp" {$x += $templist[$x].Substring(4,$templist[$x].Length -4)-1}
+            "nop" {}
+        }
+    }
+}
+until ($x -ge $templist.length)
+$acc
