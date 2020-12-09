777dd6bfa7097c4377be265a99b2a85816119038
Beardhammer <60657565+Beardhammer@users.noreply.github.com>
2020-12-09 13:25:27 -0600

    Add files via upload

---
+++ Day9/Answer1.ps1
@@ -0,0 +1,20 @@
+﻿$list = get-content "C:\Users\user\Documents\adventofcode\day9\input.txt"
+$nums = @()
+0..24|%{$nums += $list[$_]}
+for($x=25;$x -lt $list.length;$x++){
+    $current = $list[$x]
+    $lastnumbers = $nums| select -last 25
+    $trigger = $false
+    for($y=0;$y -le 24; $y++){
+
+        $half = $current-$lastnumbers[$y]
+        if($half -in $lastnumbers -and $half -ne $lastnumbers[$y]){
+            $nums+=$current
+            $trigger=$true
+            break}
+               
+        }
+        if(!($trigger)){
+            $current
+            break}
+    }
---
+++ Day9/Answer2.ps1
@@ -0,0 +1,14 @@
+﻿$list = get-content "C:\Users\matt\Documents\adventofcode\day9\input.txt"
+$final= 556543474
+$nums = @()
+for($x=0;$x -lt $list.length;$x++){
+    $nums += $list[$x]
+    if(($nums|Measure-Object -sum).sum -gt $final){
+        $x= $x-$nums.Length+1
+        $nums = @()
+        }
+    if(($nums|Measure-Object -sum).sum -eq $final){break}
+        }
+$nums = $nums | sort
+$answer = [int]$nums[0]+[int]$nums[-1]
+write-host sum is $answer
